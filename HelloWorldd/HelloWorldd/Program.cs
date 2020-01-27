98181d61869ce3698cee869ff57945d01eea37b2
connice <rodinga@mail.usf.edu>
2020-01-26 21:09:05 -0500

    Hello World

---
+++ HelloWorldd/HelloWorldd/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorldd
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Pause to display hello world!
+            //Give instructions to user
+            Console.WriteLine("Press any Key to continue..");
+            Console.WriteLine("Hello World!!");
+            Console.ReadKey(true);
+            
+        }
+    }
+}
