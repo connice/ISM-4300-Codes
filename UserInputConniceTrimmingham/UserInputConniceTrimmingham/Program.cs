a859e771ef418166a826ae12ebe9c9d513e43823
connice <rodinga@mail.usf.edu>
2020-01-31 21:07:16 -0500

    UserInputConniceTrimmingham

---
+++ UserInputConniceTrimmingham/UserInputConniceTrimmingham/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserInputConniceTrimmingham
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           //declare the variables 
+            string Answer;
+
+            //prints the greeting to users
+            Console.WriteLine(" Hi, My name is Connice Trimmingham!");
+            Console.WriteLine(" Thank You for loging in!!..What can I do for you today?");
+
+            //Ask user to enter their response and press the enter key
+            Console.WriteLine(" Please type your Request below and hit ENTER");
+           
+            //take sthe response from user 
+            Answer = Console.ReadLine();
+
+            // prints the output from user
+            Console.WriteLine(" Your request is:'{0}'", Answer);
+
+            //Press any key to exit the program 
+            Console.WriteLine(" Thank you for your request...Press any Key to exit! ");
+            Console.ReadKey(true);
+        }
+    }
+}
