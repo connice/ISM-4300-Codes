acf56df1c339dc7831be9ceca08719ff4bd5d55d
connice <rodinga@mail.usf.edu>
2020-01-31 21:21:43 -0500

    small changes to userInput

--- UserInputConniceTrimmingham/UserInputConniceTrimmingham/Program.cs
+++ UserInputConniceTrimmingham/UserInputConniceTrimmingham/Program.cs
@@ -14,20 +14,20 @@ namespace UserInputConniceTrimmingham
             string Answer;
 
             //prints the greeting to users
-            Console.WriteLine(" Hi, My name is Connice Trimmingham!");
-            Console.WriteLine(" Thank You for loging in!!..What can I do for you today?");
+            Console.WriteLine("Hi, My name is Connice Trimmingham!");
+            Console.WriteLine("Thank You for loging in....What can I do for you today?");
 
             //Ask user to enter their response and press the enter key
-            Console.WriteLine(" Please type your Request below and hit ENTER");
+            Console.WriteLine("Please type your Request below and hit ENTER");
            
             //take sthe response from user 
             Answer = Console.ReadLine();
 
             // prints the output from user
-            Console.WriteLine(" Your request is:'{0}'", Answer);
+            Console.WriteLine("Your request is:'{0}'", Answer);
 
             //Press any key to exit the program 
-            Console.WriteLine(" Thank you for your request...Press any Key to exit! ");
+            Console.WriteLine("Thank you for your request...Press any Key to exit! ");
             Console.ReadKey(true);
         }
     }
