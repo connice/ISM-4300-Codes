304aad03f6e46ca00bce413c0ca7c4a76609b2cb
connice <rodinga@mail.usf.edu>
2020-02-23 00:35:51 -0500

    additional comments to code

--- iterative Statements/iterative Statements/Program.cs
+++ iterative Statements/iterative Statements/Program.cs
@@ -11,18 +11,27 @@ namespace iterative_Statements
 
         static void Main(string[] args)
         {
+            //declare the variables 
             string n;
             int new_num;
-            new_num = 1;
+            new_num = 1; //initialize the variable newnum that we would be ussing as a counter 
 
-
+            //ask user to input number between 2 and 10
             Console.WriteLine("Enter a number between 2 and 10");
+
+            //reads user input
             n = (Console.ReadLine());
+
+            //all inputs ae string so this changes the string to and integer when user input a number 
             int N = int.Parse(n);
 
+
+            //condition of whether or not user input an even or odd number 
             if (N % 2 == 1)
 
             {
+
+                //if odd number subtract one and make it a variable newnum and spit out your have enter an odd number  newnum times
                 new_num = N - 1;
                 while (new_num > 0)
                 {
@@ -36,6 +45,8 @@ namespace iterative_Statements
             else
 
             {
+
+                //if even number add one and make it a variable newnum and spit out your have enter an odd number  newnum times
                 new_num = N + 1;
                 while (new_num > 0)
                 {
